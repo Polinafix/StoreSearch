974812fd0546f40cb319139be1b272f82d9d6eba
polinafiksson <pd.fiksson@gmail.com>
2018-04-13 16:31:31 -0500

    Added a search bar and table view

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Polina Fiksson on 13/04/2018.
+//  Copyright © 2018 PolinaFiksson. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+    
+}
